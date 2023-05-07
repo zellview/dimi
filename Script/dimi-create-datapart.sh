414a89aba6c5e01782d31eda50396c82b25e802e
kai47x <k.u.kietzke@gmail.com>
2023-05-08 00:16:46 +0200

    refine dimi-create-datapart.sh

--- Script/dimi-create-datapart.sh
+++ Script/dimi-create-datapart.sh
@@ -1,28 +1,52 @@
+
 #! bin/bash
 
 # MODULE DimiCreateDatapart;
 
-# zellview-dimi version 3.1.10 , 4. Mai 2023
+# zellview-dimi version 3.1.11 , 7. Mai 2023
 # copyright by zellview network
 
 # CONST
     Modulename="DimiCreateDatapart"
 
-# VAR
-    sourceDevice=$1
-
 # BEGIN
+    echo
+    echo "This script will create a data partition"
+    echo "at the end of the destination-device."
+    echo
+    echo "Choose the appropriate device to create the partition."
+    echo
+    lsblk
+    echo
+    read -p "Create partition on which device? /dev/" destDevice
+    destDevice=/dev/$destDevice
 
+    echo "Create partition on device"
+    echo "************"
+    echo $destDevice
+    echo "************"
+    echo
+    echo "Press RETURN to continue or CTRL+C to abort."
+    read tmp
+    
     beg=$(date +"%H:%M:%S")
+    echo "$Modulename start at $beg"
 
     # create datapartition at the end of disk
     
     #TODO
-    sudo parted /dev/sdb mkpart primary ext4 10.5GB 100%
-    sudo mkfs.ext4 /dev/sdb3
-    sudo e2label /dev/sdb3 "zv-data"
+    echo "create primary partition ext4 $destDevice 10.5 GB to 100%"
+    sudo parted $destDevice mkpart primary ext4 10.5GB 100%
     
-    
+#    echo "delete partition ${destDevice}3"
+#    sudo parted rm ${destDevice}3
+
+    echo "make filesystem ext4 on ${destDevice}3"
+    sudo mkfs.ext4 ${destDevice}3
+
+    echo "label partition ${destDevice}3"
+    sudo e2label ${destDevice}3 "zv-data"
+        
     # finnish
     end=$(date +"%H:%M:%S")
     echo "$Modulename finnished"
---
+++ Script/dimi-install-cubic.sh
@@ -0,0 +1,8 @@
+
+
+
+sudo apt-add-repository universe
+sudo apt-add-repository ppa:cubic-wizard/release
+sudo apt update
+sudo apt install --no-install-recommends cubic
+
